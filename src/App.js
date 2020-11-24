32b8ce94ad1bb5aa83ff86609d8de23c12318f33
YellowError <julieniannellopro@gmail.com>
2020-11-24 15:13:06 +0100

    reach router install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ButtonExemple from "./components/ui/ButtonExemple"
+import { Link, Router } from "@reach/router"
+import Home from "./components/pages/Home"
+import Dashboard from "./components/pages/Dashboard"
+
 const App = () => (
   <div className="py-10">
     <header>
       <h1 className="mb-10">Bienvenue sur le projet, essayons de ne pas nous entretuer !</h1>
     </header>
     <main>
-      <ButtonExemple/>
+      <nav className='mb-10'>
+        <Link className='mr-4' to="/">home</Link>
+        <Link to="dashboard">dashboard</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <Dashboard path="dashboard" />
+      </Router>
     </main>
   </div>
 )
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            HomePage
+        </div>
+    )
+}
+
+export default Home
